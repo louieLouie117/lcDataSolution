916efc5f31c0d1703fb22817f8c1a4e0a737d321
Luis Cardona <cardona-luis@outlook.com>
2024-02-19 07:39:19 -0600

    ColorUpdated

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer(props) {
   return (
     <div className='footer-container'>
-        <a href="/">LC Data Solution</a>
+        <a href="/">LC Data Solutions</a>
         
     </div>
   )
